164490e2d42ba4eaa1a07712a892e05f74438088
BeatrizChanfrone <158573894+BeatrizChanfrone@users.noreply.github.com>
2024-09-10 15:19:06 -0300

    ex06

---
+++ ex06.py
@@ -0,0 +1,15 @@
+programa
+{
+    funcao inicial()
+    {
+        inteiro numero
+
+        escreva("Digite um número para ver sua tabuada: ")
+        leia(numero)
+
+        para(inteiro i = 1; i <= 10; i++)
+        {
+            escreva(numero, " x ", i, " = ", numero * i, "\n")
+        }
+    }
+}
