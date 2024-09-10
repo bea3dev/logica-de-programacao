94ebd12f317cdd1fb15f3f7470d69e111bb1658e
BeatrizChanfrone <158573894+BeatrizChanfrone@users.noreply.github.com>
2024-09-10 15:13:46 -0300

    ex05

---
+++ ex05.py
@@ -0,0 +1,14 @@
+programa
+{
+    funcao inicio()
+    {
+        real celcius, fahrenheit
+
+        escreva("Digite a temperatura em celsius: ")
+        leia(celsius)
+
+        fahrenheit = (celsius * 9/5) + 32
+
+        escreva("A temperatura em fahrenheit é: ", fahrenheit)
+    }
+}
